3f2b9b076887dd6be017881d31a7030b2bd17a01
SebastianSarasti <sebastiansarasti.14@gmail.com>
2024-02-28 18:58:45 -0500

    inicio GUI

--- AntBot/src/App.java
+++ AntBot/src/App.java
@@ -7,11 +7,6 @@
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
-import com.formdev.flatlaf.FlatLightLaf;
-import com.formdev.flatlaf.intellijthemes.FlatArcOrangeIJTheme;
-import com.formdev.flatlaf.intellijthemes.materialthemeuilite.FlatArcDarkIJTheme;
-import com.formdev.flatlaf.intellijthemes.materialthemeuilite.FlatAtomOneDarkIJTheme;
-
 import UserInterface.Form.MainForm;
 import UserInterface.Form.SplashScreenForm;
 
