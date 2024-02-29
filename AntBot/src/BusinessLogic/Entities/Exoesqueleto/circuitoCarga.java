a1626f079075407de22ec8ba7aa860bdf8721ed4
Julian <alekseinavalforever@gmail.com>
2024-02-29 07:12:10 -0500

    ejercito

--- AntBot/src/BusinessLogic/Entities/Exoesqueleto/circuitoCarga.java
+++ AntBot/src/BusinessLogic/Entities/Exoesqueleto/circuitoCarga.java
@@ -5,7 +5,7 @@ public class CircuitoCarga {
     private Integer amperios = 200;
 
     void cargarse(){
-        System.out.println("El AntBot se esta cargando");
+        System.out.println("El AntBot se esta cargando con un voltaje de " + voltaje +" la bateria es de " + amperios + "amperios");
     }
 
 }
