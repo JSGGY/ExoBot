698362e9e0c9c9005fb79fff367eca395cc1802a
Dylan IMM <dylan.maldonado@epn.edu.ec>
2024-02-29 02:48:26 -0500

    PantallaHormigaIdioma

--- AntBot/src/BusinessLogic/Entities/EjercitoRuso/HormigaRusa.java
+++ AntBot/src/BusinessLogic/Entities/EjercitoRuso/HormigaRusa.java
@@ -5,49 +5,20 @@ import BusinessLogic.Entities.Exoesqueleto.TurboReactor;
 
 public class HormigaRusa extends Hormiga implements IhomigaExtremidad {
     private String clasificacion;
-    
-    private Boolean ssHabilidadBrazoIzquierdo;
-    private Boolean ssHabilidadBrazoDerecho;
-    
+        
     public HormigaRusa(String nombre, String clasificacion) {
         super(nombre);
         this.clasificacion = clasificacion;
-        if (HormigaRusa.radio)
-        setssHabilidadBrazoDerecho(getHabilidadRandom());
-        
-        if (HormigaRusa.circuitoAnillado)
-        setssHabilidadBrazoIzquierdo(getHabilidadRandom());
-        
-    }
-    
-    private boolean getHabilidadRandom() {
-        int bias = 50;
-        return (Math.random() * 100 < bias);
-    }
-    
-    public Boolean getssHabilidadBrazoIzquierdo() {
-        return ssHabilidadBrazoIzquierdo;
     }
     
     public String getClasificacion() {
         return clasificacion;
     }
 
-    public void setClasificacion(String clasificacion) {
-        this.clasificacion = clasificacion;
-    }
+    public void setClasificacion(String clasificacion) {            
+            this.clasificacion = clasificacion;
+        }
     
-    public void setssHabilidadBrazoIzquierdo(Boolean ssHabilidadBrazoIzquierdo) {
-        this.ssHabilidadBrazoIzquierdo = ssHabilidadBrazoIzquierdo;
-    }
-
-    public Boolean getssHabilidadBrazoDerecho() {
-        return ssHabilidadBrazoDerecho;
-    }
-
-    public void setssHabilidadBrazoDerecho(Boolean ssHabilidadBrazoDerecho) {
-        this.ssHabilidadBrazoDerecho = ssHabilidadBrazoDerecho;
-    }
 
     public void ssReemplazarBateriaTurboReactor(TurboReactor oSSTurboReactor) {
         System.out.println("Recargando/ reemplazando batería");
